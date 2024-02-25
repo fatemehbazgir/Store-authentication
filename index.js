d78459153acbbbcfaf5ea0a76fc8f497c872bcea
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-02-25 21:45:24 +0330

    Showing products on the site and styling

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
 import { getCookie } from "./utils/cookie.js";
 import { getData } from "./utils/httpReq.js";
+import { shortenText } from "./utils/stringFunc.js";
 
 const loginButton = document.getElementById("login");
 const dashboardButton = document.getElementById("dashboard");
+const mainContent = document.getElementById("products");
 
+// Showing products on the site
+const showProduct = (products) => {
+  mainContent.innerHTML = "";
 
+  products.forEach((product) => {
+    const jsx = `
+    <div>
+    <img src=${product.image} alt=${product.title} />
+    <h4>${shortenText(product.title)}</h4>
+    <div id="price">
+    <p>$ ${product.price}</p>
+    <button>
+    Buy
+    <i class="fa-solid fa-cart-shopping"></i>
+    </button>
+    </div>
+    <div id="rate">
+    <i class="fa-solid fa-star"></i>
+    <span>${product.rating.rate}</span>
+    </div>
+    <div id="count">
+    <i class="fa-solid fa-user"></i>
+    <span>${product.rating.count}</span>
+    </div>
+    </div>
+    `;
+
+    mainContent.innerHTML += jsx;
+  });
+};
 
 // Show login and dashboard buttons based on user status
-const init = async() => {
+const init = async () => {
   const cookie = getCookie();
 
   if (cookie) {
@@ -16,12 +47,8 @@ const init = async() => {
     dashboardButton.style.display = "none";
   }
 
-  
-  const allProducts=await getData("products")
-  
+  const allProducts = await getData("products");
+  showProduct(allProducts);
 };
-
-
-
 
 document.addEventListener("DOMContentLoaded", init);
---
+++ utils/stringFunc.js
@@ -0,0 +1,6 @@
+// Shorten product titles
+const shortenText = (text) => {
+  return text.split(" ").slice(0, 3).join(" ");
+};
+
+export {shortenText}
