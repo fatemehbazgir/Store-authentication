816233a69b13a092cd8b9c9274c585c515cc8b94
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-02-29 22:43:03 +0330

    Getting user information

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,18 @@
 import authHandler from "./utils/authorization.js";
+import { getData } from "./utils/httpReq.js";
 
-const init = () => {
+const mainContent = document.getElementById("container");
+
+const renderUsers = () => {
+  mainContent.innerHTML = "";
+};
+
+const init = async () => {
   authHandler();
+
+  // Getting user information
+  const users = await getData("users");
+  renderUsers(users);
 };
 
 document.addEventListener("DOMContentLoaded", init);
