750e9bb3c0292f4240307c8f1f5effe63a1e5569
Fatemeh Bazgir <fatemehbazgir2018@gmail.com>
2024-02-29 23:04:33 +0330

    Show user information

--- dashboard.js
+++ dashboard.js
@@ -3,8 +3,39 @@ import { getData } from "./utils/httpReq.js";
 
 const mainContent = document.getElementById("container");
 
-const renderUsers = () => {
+const renderUsers = (users) => {
   mainContent.innerHTML = "";
+
+  // Show user information
+  users.forEach((user) => {
+    const jsx = `
+    <div id="card">
+    <h3>${user.id}</h3>
+    <div>
+    <p><i class="fa-solid fa-user"></i>Name:</p>
+    <span>${user.name.firstname} ${user.name.lastname}</span>
+    </div>
+    <div>
+    <p><i class="fa-solid fa-paperclip"></i>UserName:</p>
+    <span>${user.username}</span>
+    </div>
+    <div>
+    <p><i class="fa-solid fa-envelope"></i>Email:</p>
+    <span>${user.email}</span>
+    </div>
+    <div>
+    <p><i class="fa-solid fa-phone"></i>Phone:</p>
+    <span>${user.phone}</span>
+    </div>
+    <div>
+    <p><i class="fa-solid fa-location-dot"></i>Address:</p>
+    <span>${user.address.city} - ${user.address.street} - ${user.address.zipcode}</span>
+    </div>
+    </div>
+    `;
+
+    mainContent.innerHTML += jsx;
+  });
 };
 
 const init = async () => {
